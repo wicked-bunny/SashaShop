51b4d097ba45c0481fce9d79f880218634ee60aa
WickedBunny <yd@p-i-u.de>
2026-06-09 11:01:14 +0200

    Sasha_Shop:added a IProductrepository interface

---
+++ Interfaces.py
@@ -0,0 +1,9 @@
+from abc import ABC, abstractmethod
+from typing import List, Dict, Any
+
+class IProductRepository(ABC):
+
+    @abstractmethod
+    def get_all_products(self) -> List[Dict[str, Any]]:
+        """Returns a list of all products in the form of dictionaries"""
+        pass
