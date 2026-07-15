dc4f0f0a764150d445765e1a04a4a3aebcd97356
WickedBunny <yd@p-i-u.de>
2026-07-15 22:36:40 +0200

    fix(db): add missing logic to update function.

--- repositories/implementations/sql_alchemie_repository.py
+++ repositories/implementations/sql_alchemie_repository.py
@@ -1,7 +1,6 @@
 from typing import TypeVar, Type, Optional, List
 
-
-from sqlalchemy import select, ColumnElement
+from sqlalchemy import select, update
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from database.models import BaseModel
@@ -23,10 +22,24 @@ class SqlAlchemyRepository(IRepository[T]):
 
         id_column = getattr(self.model,"id")
 
-        query = select(self.model).where(id_column == obj_id)
-        result = await self.session.execute(query)
+        stmt = select(self.model).where(id_column == obj_id)
+        result = await self.session.execute(stmt)
 
         return result.scalar_one_or_none()
+
+    async def update_by_id(self, obj_id: int, data: dict) -> bool:
+        id_column = getattr(self.model, "id")
+
+        stmt = (
+            update(self.model)
+            .where(id_column == obj_id)
+            .values(**data)
+            .execution_options(synchronize_session="fetch")
+        )
+        result = await self.session.execute(stmt)
+
+        # rowcount returns the number of modified rows
+        return result.rowcount > 0
 
     async def add(self, entity: T) -> T:
         self.session.add(entity)
